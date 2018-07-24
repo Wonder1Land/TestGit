ee42a54dfd0aff2fc58ccf53bf438a6227f6ffd5
Wonder1Land <zlbwonder@yeah.net>
2018-07-24 16:09:25 +0800

    用idea添加

---
+++ src/main/java/com/boot/girl/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.boot.girl;
+
+/**
+ * 类名:HelloWorld
+ * 作者:朱利斌
+ * 日期:2018/7/24 16:08
+ * 说明:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
