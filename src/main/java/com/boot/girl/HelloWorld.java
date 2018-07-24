06a4f3993b8f187d36de5e18953c465de8cf609b
Wonder1Land <zlbwonder@yeah.net>
2018-07-24 16:20:16 +0800

    用idea添加2

--- src/main/java/com/boot/girl/HelloWorld.java
+++ src/main/java/com/boot/girl/HelloWorld.java
@@ -8,6 +8,6 @@ package com.boot.girl;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
     }
 }
