2f192de9f7f716560f3680e96b25456939da38dd
Wonder1Land <zlbwonder@yeah.net>
2019-10-31 16:11:58 +0800

    用idea添加2

--- src/main/java/com/boot/girl/TimeTask.java
+++ src/main/java/com/boot/girl/TimeTask.java
@@ -14,7 +14,7 @@ import java.util.Date;
  */
 @Component
 public class TimeTask {
-    @Scheduled(cron = "0/5 * * * * ?")
+   // @Scheduled(cron = "0/5 * * * * ?")
      public void task(){
         SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(sdf.format(new Date()));
